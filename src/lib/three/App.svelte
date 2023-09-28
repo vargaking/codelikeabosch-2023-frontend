7933800adcd427e0061d1c0200b6186147e22366
Daniel Gergely <daniel.gergely.2005@gmail.com>
2023-09-28 19:18:43 +0200

    init threlte

---
+++ src/lib/three/App.svelte
@@ -0,0 +1,8 @@
+<script>
+	import { Canvas } from '@threlte/core';
+	import Scene from './Scene/Scene.svelte';
+</script>
+
+<Canvas>
+	<Scene />
+</Canvas>
--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,9 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
-import mkcert from 'vite-plugin-mkcert';
 
 export default defineConfig({
-	plugins: [mkcert(), sveltekit()],
+	plugins: [sveltekit()],
 	ssr: {
-		noExternal: ['three']
-	}
+		noExternal: ['three'],
+	},
 });
