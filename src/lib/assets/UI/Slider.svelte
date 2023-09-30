e3a316ec80f10d1676a95217bc08acf6d940e3d1
Zsombor Horvath <horvathzsombor2005@gmail.com>
2023-09-30 03:06:36 +0200

    Add type for HTML selectelement

--- src/lib/assets/UI/Slider.svelte
+++ src/lib/assets/UI/Slider.svelte
@@ -5,8 +5,11 @@
 
 	$: value = ((world[$tick].time - world[0].time) / maxTime) * 100;
 
-	function handleChange(event) {
-		let time = (event.target.value / 100) * maxTime;
+	function handleChange(event: Event) {
+		let target = event.target as HTMLInputElement;
+		if(!target) return;
+
+		let time = (parseInt(target.value) / 100) * maxTime;
 
 		for (let i = 0; i < world.length; i++) {
 			if (world[i].time > time + world[0].time) {
