dc3f0a4bef0f05979d0a282ca1271f580f1b9130
Daniel Gergely <daniel.gergely.2005@gmail.com>
2023-09-30 03:39:30 +0200

    fix slider

--- src/lib/assets/UI/Slider.svelte
+++ src/lib/assets/UI/Slider.svelte
@@ -1,30 +1,18 @@
 <script lang="ts">
 	import { sliderTick, tick, world, isPlaying } from '$lib/stores';
 
-	let maxTime: number;
+	$: value = (($world[$tick].time - $world[0].time) / maxTime) * 100;
 
-	let value: number;
-
-	$: {
-		try {
-			value = ((world[$tick].time - world[0].time) / maxTime) * 100;
-		} catch {
-			value = 0;
-		}
-	}
-
-	if (world.length > 0) {
-		maxTime = world[world.length - 1].time - world[0].time;
-	}
+	let maxTime = $world[$world.length - 1].time - $world[0].time;
 
 	function handleChange(event: Event) {
 		let target = event.target as HTMLInputElement;
-		if(!target) return;
+		if (!target) return;
 
 		let time = (parseInt(target.value) / 100) * maxTime;
 
-		for (let i = 0; i < world.length; i++) {
-			if (world[i].time > time + world[0].time) {
+		for (let i = 0; i < $world.length; i++) {
+			if ($world[i].time > time + $world[0].time) {
 				$sliderTick = i;
 				break;
 			}
